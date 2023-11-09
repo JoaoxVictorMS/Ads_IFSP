c51b1364403546c4b4697198400d12977b723058
João Victor <98715957+JoaoxVictorMS@users.noreply.github.com>
2023-11-09 15:15:26 -0300

    Add files via upload

---
+++ Linguagens de Programação/C#/Aula18_entrada_saida/Aula18_entrada_saida/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula18_entrada_saida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("João");
+            Console.Write("IFSP");
+            
+
+            int codigo = Console.Read(); // Read() irá capturar apenas o primeiro valor
+            Console.WriteLine(codigo); // Será impresso o valor do número correspondente na tabela ASCII. Ex: 50 = 53
+            */
+
+            // Aqui será impresso na faixa
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
